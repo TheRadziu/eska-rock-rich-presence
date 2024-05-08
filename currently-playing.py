f280bbc765ae61b747d6e9856e8104107a610ffa
TheRadziu <TheRadziu@users.noreply.github.com>
2024-05-08 17:09:26 +0200

    Update to v1.0.4
    Definitive fix for songs looping when ads/speaker is talking.
    Ostateczny fix na loopowanie się piosenek gdy lecą reklamy/speaker mówi.

--- currently-playing.py
+++ currently-playing.py
@@ -1,6 +1,6 @@
 ### Eska Rock Rich Presence
 ## By TheRadziu
-# v1.0.3
+# v1.0.4
 import requests
 import json
 from pypresence import Presence
@@ -12,6 +12,21 @@ wait_time = 15 # Conajmniej 15
 URL = "https://player.eskarock.pl/api/mobile/station/5980/now_playing/" # Aby zmienić stację na inną podmień jej ID
 client_id = "1148784842494988358" # ID Aplikacji, w tym wypadku "Eska Rock"
 ############
+
+class LimitedDict:
+    def __init__(self, max_size=3):
+        self.max_size = max_size
+        self.dict = {}
+
+    def add_pair(self, key, value):
+        if key not in self.dict:
+            if len(self.dict) >= self.max_size:
+                oldest_key = next(iter(self.dict))
+                del self.dict[oldest_key]
+            self.dict[key] = value
+
+    def __str__(self):
+        return str(self.dict)
 
 def parse_api():
     r = requests.get(url=URL)
@@ -25,15 +40,15 @@ def parse_api():
                 songs['thumb'] = "https://cdn.music.smcloud.net/t/cover/02c59e2d-798d-4e47-9d80-93e451f5b5ed_icon-ROCK-NEW-eska_500x500.png"
             return songs['thumb'], songs['artists'][0]['name'], songs['name']
 
+last_3_songs = LimitedDict()
 RPC = Presence(client_id=client_id)
 RPC.connect()
 start_time=time.time()
-last_song = ""
 
 while "man" != "woman":
     parse_api_once = parse_api()
-    if last_song == "" or last_song != parse_api_once[2]:
-        last_song = parse_api_once[2]
+    if parse_api_once[2] not in last_3_songs.dict:
+        last_3_songs.add_pair(parse_api_once[2], parse_api_once[1])
         print("Currently playing: "+parse_api_once[2]+" by "+parse_api_once[1])
         RPC.update(large_image=parse_api_once[0],
             details=parse_api_once[1],
